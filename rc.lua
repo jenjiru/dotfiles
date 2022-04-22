16856d9911ca4e70706681eace399ed34522319a
Prihler <94796499+Prihler@users.noreply.github.com>
2022-04-22 22:59:29 +0200

    .

--- rc.lua
+++ rc.lua
@@ -562,3 +562,10 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Gaps
+beautiful.useless_gap = 5
+
+-- autostart
+--awful.spawn.with_shell("picom")
+awful.spawn.with_shell("nitrogen --restore")
