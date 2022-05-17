0ed75434030712bbf916b43ad0fa70529602c02d
Prihler <Prihler@users.noreply.github.com>
2022-05-17 22:05:14 +0200

    Update aether-theme-setup.sh

--- aether-theme-setup.sh
+++ aether-theme-setup.sh
@@ -2,6 +2,6 @@
 
 echo "Updating Login-Manager theme and rebooting"
 sudo cp -r ~/umi-tmp/localstorage/ /var/lib/lightdm/.local/share/webkitgtk/
-sudo rm /opt/scripts/aether-theme-setup
-sed -i '/awful.spawn.with_shell("alacritty -e /opt/scripts/aether-theme-setup.sh")/d' ~/.config/awesome/rc.lua
+sudo rm /opt/scripts/aether-theme-setup.sh
+sed -i '/aether/d' ~/.config/awesome/rc.lua
 reboot
