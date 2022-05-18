8dd280fbaec058aa5c6f25f5592a97e4befc0c46
Prihler <Prihler@users.noreply.github.com>
2022-05-18 17:03:51 +0200

    Update aether-theme-setup.sh

--- aether-theme-setup.sh
+++ aether-theme-setup.sh
@@ -2,6 +2,7 @@
 
 echo "Updating Login-Manager theme and rebooting"
 sudo cp -r ~/umi-tmp/localstorage/ /var/lib/lightdm/.local/share/webkitgtk/localstorage/
+sudo touch /var/lib/lightdm/.local/share/webkitgtk/localstorage/file___0.localstorage-wal
 sudo rm /opt/scripts/aether-theme-setup.sh
 sudo rm -r ~/umi-tmp
 sed -i '/aether/d' ~/.config/awesome/rc.lua
