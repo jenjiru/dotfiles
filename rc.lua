e1a419f60a868b6333323c0489403a095082695b
Prihler <Prihler@users.noreply.github.com>
2022-05-22 22:20:44 +0200

    .

--- rc.lua
+++ rc.lua
@@ -316,6 +316,10 @@ globalkeys = gears.table.join(
     awful.spawn.with_shell("rofi -show drun") end,
               {description = "run prompt", group = "launcher"}),
 
+    awful.key({ modkey, "Shift"   },            "r",     function ()
+    awful.spawn.with_shell("sudo rofi -show drun") end,
+              {description = "sudo run prompt", group = "launcher"}),
+
     awful.key({ modkey }, "x",
               function ()
                   awful.prompt.run {
