b32cc8cd4336ccad85c4bfc9ba0166e9b20b8f30
prihler <94796499+Prihler@users.noreply.github.com>
2022-07-17 19:16:33 +0200

    .

--- arch-iso.sh
+++ arch-iso.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+# Script do torrent the latest arch iso
 
+day=$(date +%d)
+month=$(date +%m)
+
+cd ~/Downloads
+sleep 2
+rm ~/Downloads/arch.iso.torrent
+sleep 5
+curl 'https://archlinux.org/releng/releases/2022.07.01/torrent/' -o arch.iso.torrent
+
+echo "it is the $day.$month"
+#date +%d 
+#date +%m
