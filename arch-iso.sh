fbe1ecbd35b4af28fe8a704a7887d55a8d253a87
prihler <94796499+Prihler@users.noreply.github.com>
2022-07-17 22:04:22 +0200

    Update arch-iso.sh

--- arch-iso.sh
+++ arch-iso.sh
@@ -5,10 +5,13 @@
 #day=$(date +%d)
 #month=$(date +%m)
 
-cd ~/Downloads
+cd /home/$USER/Downloads
 
 curl -s 'https://archlinux.org/releng/releases/2022.07.01/torrent/' -o arch.iso.torrent
-aria2c ~/Downloads/arch.iso.torrent --seed-time=0.01 -d ~/Downloads
-rm ~/Downloads/arch.iso.torrent
-notify-send "  Arch-iso" "downloaded  " -t 4000 -i ~/Pictures/50x50-arch.png
-#kill -25 $PPID
+aria2c /home/$USER/Downloads/arch.iso.torrent --seed-time=0.01 -d /home/$USER/Downloads
+
+rm /home/$USER/Downloads/arch.iso.torrent
+
+notify-send "  Arch-iso" "downloaded  " -t 4000 -i /home/$USER/Pictures/50x50-arch.png
+
+kill -25 $PPID
