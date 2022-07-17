0fc49d31df6eb2a4cd0555ce30fa3560f966bb57
prihler <94796499+Prihler@users.noreply.github.com>
2022-07-17 19:58:08 +0200

    Update arch-iso.sh

--- arch-iso.sh
+++ arch-iso.sh
@@ -2,15 +2,13 @@
 
 # Script do torrent the latest arch iso
 
-day=$(date +%d)
-month=$(date +%m)
-sleep 2
+#day=$(date +%d)
+#month=$(date +%m)
+
 cd ~/Downloads
+
 curl -s 'https://archlinux.org/releng/releases/2022.07.01/torrent/' -o arch.iso.torrent
-sleep 10
+
 rm ~/Downloads/arch.iso.torrent
 notify-send "Arch iso downlaod is done." -t 4000
-echo "it is the $day.$month"
-#date +%d 
-#date +%m
-kill -9 $var
+#kill -25 $PPID
