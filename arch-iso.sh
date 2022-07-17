2e10daf98a95c49c1bfebf6f9fdcd1a98edd6d62
prihler <94796499+Prihler@users.noreply.github.com>
2022-07-17 21:17:51 +0200

    Update arch-iso.sh

--- arch-iso.sh
+++ arch-iso.sh
@@ -10,5 +10,5 @@ cd ~/Downloads
 curl -s 'https://archlinux.org/releng/releases/2022.07.01/torrent/' -o arch.iso.torrent
 aria2c ~/Downloads/arch.iso.torrent --seed-time=0.01 -d ~/Downloads
 rm ~/Downloads/arch.iso.torrent
-notify-send "Arch iso downlaod is done." -t 4000
+notify-send "  Arch-iso" "downloaded  " -t 4000 -i ~/Pictures/50x50-arch.png
 #kill -25 $PPID
