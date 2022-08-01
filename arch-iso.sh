a04e8835d9e0b20880c0a8a1e62c4adf671f32aa
Prihler <94796499+Prihler@users.noreply.github.com>
2022-08-01 13:08:46 +0200

    Update arch-iso.sh

--- arch-iso.sh
+++ arch-iso.sh
@@ -2,18 +2,43 @@
 
 # Script do torrent the latest arch iso
 
-#day=$(date +%d)
-#month=$(date +%m)
-#year=$(date +%y)
+output=$HOME/music
+#echo "$output"
+
+day=$(date +%d)
+month=$(date +%m)
+year=$(date +%y)
+
+cd $output
+
+day=01
+month=07
+
+echo "https://archlinux.org/releng/releases/20$year.$month.$day/torrent/"
+
+#until curl -fs 'https://archlinux.org/releng/releases/20$year.$month.$day/torrent/' -o arch.iso.torrent; do
+#  echo "There is no version from the $day/$month/$year"
+#  sleep 4
+#done
 
 
-cd /home/$USER/downloads
+curl -fs 'https://archlinux.org/releng/releases/20$year.$month.$day/torrent/' -o arch.iso.torrent
 
-curl -s 'https://archlinux.org/releng/releases/2022.07.01/torrent/' -o arch.iso.torrent
-aria2c /home/$USER/downloads/arch.iso.torrent --seed-time=0.01 -d /home/$USER/downloads
+#curl -fs 'https://archlinux.org/releng/releases/2022.07.01/torrent/' -o arch.iso.torrent
+aria2c $output/arch.iso.torrent --seed-time=0.01 -d $output
 
-rm /home/$USER/downloads/arch.iso.torrent
+
+
+
+
+
+
+
+
+
+
+rm $output/arch.iso.torrent
 
 notify-send "  Arch-iso" "downloaded  " -t 4000 -i /home/$USER/pictures/scripts-pic/notify-arch.png
 
-kill -25 $PPID
+#kill -25 $PPID
