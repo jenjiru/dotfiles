ca51429c56587baccef70afd3e1d37b3c56c4276
Prihler <94796499+Prihler@users.noreply.github.com>
2022-08-07 22:28:44 +0200

    Update wallpaper-setup.sh

--- wallpaper-setup.sh
+++ wallpaper-setup.sh
@@ -3,5 +3,5 @@
 nitrogen --set-zoom-fill --save $HOME/pictures/wallpaper/asian/doge.png
 
 # removing the script
-sudo rm /opt/wallpaper-setup.sh
+sudo rm $HOME/wallpaper-setup.sh
 sed -i '/wallpaper-setup.sh/d' $HOME/.config/awesome/rc.lua
