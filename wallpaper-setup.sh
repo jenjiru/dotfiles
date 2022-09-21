def0614bf31cdec3f159d1d754aa7163bf958c15
Prihler <94796499+Prihler@users.noreply.github.com>
2022-09-21 17:23:16 +0200

    Delete wallpaper-setup.sh

--- wallpaper-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-nitrogen --set-zoom-fill --save $HOME/pictures/wallpaper/asian/doge.png
-
-# removing the script
-rm $HOME/wallpaper-setup.sh
-sed -i '/wallpaper-setup.sh/d' $HOME/.config/awesome/rc.lua
