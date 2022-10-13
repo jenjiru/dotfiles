62895725f5ec84a151bc75dca6e063231fe9691d
Prihler <94796499+Prihler@users.noreply.github.com>
2022-10-13 22:26:12 +0200

    Update rc.lua

--- rc.lua
+++ rc.lua
@@ -516,12 +516,17 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
--- autostart
-awful.spawn.with_shell("/opt/wm-program-check.sh")
+-- ---autostart---
+-- -programs-
 --awful.spawn.with_shell("redshift")
 awful.spawn.with_shell("flameshot")
-awful.spawn.with_shell("xrandr --output Virtual-1 --mode 1920x1080")
---awful.spawn.with_shell("alacritty -e sudo /opt/timeshift-setup.sh")
-awful.spawn.with_shell("~/.fehbg")
+awful.spawn.with_shell("/opt/wm-program-check.sh")
 awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+-- -bg-
+awful.spawn.with_shell("~/.fehbg")
+-- -other-
+--awful.spawn.with_shell("alacritty -e sudo /opt/timeshift-setup.sh")
+-- -compositor-
 awful.spawn.with_shell("picom")
+-- -monitor-
+awful.spawn.with_shell("xrandr --output Virtual-1 --mode 1920x1080")
