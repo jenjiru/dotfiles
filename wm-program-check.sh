87dff086484399c4d69364aea7f3e3bbada5391d
Prihler <94796499+Prihler@users.noreply.github.com>
2022-10-27 20:27:05 +0200

    Update wm-program-check.sh

--- wm-program-check.sh
+++ wm-program-check.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# starting programms on awesomewm (re)start only when not already running
+# starting programms on awesomewm (re)start only when not already running :/
 
-# solaar
-slc=$(ps aux | grep solaar > /dev/null | wc -l) && if [ $slc -eq 1 ]; then solaar -w hide &;fi
+# redshift
+rlc=$(ps aux | grep redshift > /dev/null | wc -l) && if [ $rlc -eq 1 ]; then redshift &; fi
