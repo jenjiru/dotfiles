e64c0f2c4bff2d2cd7f847c34fa53332cadb917e
Prihler <94796499+Prihler@users.noreply.github.com>
2024-03-05 02:08:30 +0000

    Update 51-alsa-disable.lua

--- 51-alsa-disable.lua
+++ 51-alsa-disable.lua
@@ -1,12 +1,28 @@
-rule = {
-  matches = {
+-- Rule to disable devices from Advanced Micro Devices, Inc. [AMD/ATI]
+local amdRule = {
+ matches = {
     {
       { "device.vendor.name", "equals", "Advanced Micro Devices, Inc. [AMD/ATI]" },
     },
-  },
-  apply_properties = {
+ },
+ apply_properties = {
     ["device.disabled"] = true,
-  },
+ },
 }
 
-table.insert(alsa_monitor.rules,rule)
+table.insert(alsa_monitor.rules, amdRule)
+
+-- Rule to disable devices from Micro Star International
+local msiRule = {
+ matches = {
+    {
+      { "device.vendor.name", "equals", "Micro Star International" },
+    },
+ },
+ apply_properties = {
+    ["device.disabled"] = true,
+ },
+}
+
+table.insert(alsa_monitor.rules, msiRule)
+
